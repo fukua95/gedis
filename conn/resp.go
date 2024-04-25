3715dc5e73bc87e8021437c7b365d1094b71e8cd
fukua95 <fukua95@gmail.com>
2024-04-25 16:18:34 +0800

    RESP definition

---
+++ conn/resp.go
@@ -0,0 +1,20 @@
+package conn
+
+// redis resp protocol data type.
+const (
+	RespStatus    = '+' // +<string>\r\n
+	RespError     = '-' // -<string>\r\n
+	RespString    = '$' // $<length>\r\n<bytes>\r\n
+	RespInt       = ':' // :<number>\r\n
+	RespNil       = '_' // _\r\n
+	RespFloat     = ',' // ,<float>\r\n
+	RespBool      = '#' // true: #t\r\n false: #f\r\n
+	RespBlobError = '!' // !<length>\r\n<bytes>\r\n
+	RespVerbatim  = '=' // =<length>\r\nFORMAT:<bytes>\r\n
+	RespBigInt    = '(' // (<big number>\r\n
+	RespArray     = '*' // //*<len>\r\n
+	RespMap       = '%' // %<len>\r\n
+	RespSet       = '~' // ~<len>\r\n
+	RespAttr      = '|' // |<len>\r\n(key)\r\n(value)\r\n... + command reply
+	RespPush      = '>' // ><len>\r\n
+)
