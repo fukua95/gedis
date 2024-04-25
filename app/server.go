db02d65694e58d7cd72d59a760cb7a3b7e97c53e
fukua95 <fukua95@gmail.com>
2024-04-25 16:03:01 +0800

    handle concurrent PING

--- app/server.go
+++ app/server.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer l.Close()
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	fmt.Println("Server start to accept requests")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Println("Error reading from conn: ", err.Error())
+			return
+		}
+		res := []byte("+PONG\r\n")
+		n, err := conn.Write(res)
+		if err != nil || n != len(res) {
+			fmt.Println("Error writing to conn: ", err.Error())
+		}
+	}
 }
